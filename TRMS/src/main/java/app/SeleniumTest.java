956851d16dcab7ac9b4f87da3b0792eea80958f0
HuangyingruiWang <whyrchris@hotmail.com>
2021-11-02 09:38:54 -0500

    new file:   TRMS/.idea/vcs.xml 	modified:   TRMS/pom.xml 	modified:   TRMS/src/main/java/app/SeleniumTest.java 	modified:   TRMS/src/main/java/pages/LoginPage.java 	modified:   TRMS/src/main/resources/logfile.log 	deleted:    TRMS/target/classes/app/SeleniumTest.class 	modified:   TRMS/target/classes/logfile.log

--- TRMS/src/main/java/app/SeleniumTest.java
+++ TRMS/src/main/java/app/SeleniumTest.java
@@ -1,34 +1,34 @@
-package app;
-
-
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import pages.LoginPage;
-
-import java.util.concurrent.TimeUnit;
-
-public class SeleniumTest {
-    public static void main(String[] args) {
-        WebDriver driver = null;
-        try {
-        String driverPath = "C:/Work/chromedriver.exe";
-        System.setProperty("webdriver.chrome.driver",driverPath);
-        driver = new ChromeDriver();
-        LoginPage loginPage = new LoginPage(driver);
-
-        String url = "C:/Work/HTML&CSS/project1/LogIn.html";
-        driver.get(url);
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-        loginPage.name.sendKeys("CS");
-        loginPage.pass.sendKeys("CS123");
-        loginPage.loginBtn.click();
-
-        Thread.sleep(2000);
-        } catch (NoSuchElementException | InterruptedException e) {
-            e.printStackTrace();
-        }
-        if (driver != null) driver.quit();
-    }
-}
+//package app;
+//
+//
+//import org.openqa.selenium.NoSuchElementException;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import pages.LoginPage;
+//
+//import java.util.concurrent.TimeUnit;
+//
+//public class SeleniumTest {
+//    public static void main(String[] args) {
+//        WebDriver driver = null;
+//        try {
+//        String driverPath = "C:/Work/chromedriver.exe";
+//        System.setProperty("webdriver.chrome.driver",driverPath);
+//        driver = new ChromeDriver();
+//        LoginPage loginPage = new LoginPage(driver);
+//
+//        String url = "C:/Work/HTML&CSS/project1/LogIn.html";
+//        driver.get(url);
+//        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//
+//        loginPage.name.sendKeys("CS");
+//        loginPage.pass.sendKeys("CS123");
+//        loginPage.loginBtn.click();
+//
+//        Thread.sleep(2000);
+//        } catch (NoSuchElementException | InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        if (driver != null) driver.quit();
+//    }
+//}
