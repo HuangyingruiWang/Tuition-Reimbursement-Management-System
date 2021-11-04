e6bc23f0c7bdc3d387a8c2222b41ec628a06dbef
HuangyingruiWang <whyrchris@hotmail.com>
2021-11-04 16:56:56 -0500

    new update

--- TRMS API/src/main/java/util/HibernateUtil.java
+++ TRMS API/src/main/java/util/HibernateUtil.java
@@ -9,7 +9,7 @@ public class HibernateUtil {
     //Abstract away creating our SessionFactory and the process of creating Sessions
     //from that SessionFactory
 
-    private static SessionFactory sf = new Configuration().configure().buildSessionFactory();
+    private static SessionFactory sf = new Configuration().setProperty("hibernate.connection.url", System.getenv("DB_URL")).setProperty("hibernate.connection.username", System.getenv("DB_USERNAME")).setProperty("hibernate.connection.password", System.getenv("DB_PASSWORD")).configure().buildSessionFactory();
 
     public static Session getSession() {
         return sf.openSession();
